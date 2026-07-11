1508571f78b50599d5f51cb8343b16f1119d5b8d
itcast <hello@itcast.cn>
2026-07-11 20:04:24 +0800

    fix: Remove SpringBootTest from huixiang-pojo module

--- 后端/HuiXiangLife/huixiang-pojo/src/test/java/com/huixiang/HuixiangPojoApplicationTests.java
+++ 后端/HuiXiangLife/huixiang-pojo/src/test/java/com/huixiang/HuixiangPojoApplicationTests.java
@@ -1,13 +1,5 @@
 package com.huixiang;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class HuixiangPojoApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
