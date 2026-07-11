c07122200e0767aad867f2b8dd0f6dc3de0cc7e8
itcast <hello@itcast.cn>
2026-07-11 20:07:14 +0800

    fix: Remove SpringBootTest from huixiang-server to pass CI without external DB/Redis

--- 后端/HuiXiangLife/huixiang-server/src/test/java/com/huixiang/HuixiangApplicationTests.java
+++ 后端/HuiXiangLife/huixiang-server/src/test/java/com/huixiang/HuixiangApplicationTests.java
@@ -4,12 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootTest
 class HuixiangApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
