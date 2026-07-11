ce8ea4dcaa0d37f8c0d73a1597837b5abd886267
itcast <hello@itcast.cn>
2026-07-11 20:00:54 +0800

    fix: Remove SpringBootTest from huixiang-common module

--- 后端/HuiXiangLife/huixiang-common/src/test/java/com/huixiang/huixiangcommon/HuixiangCommonApplicationTests.java
+++ 后端/HuiXiangLife/huixiang-common/src/test/java/com/huixiang/huixiangcommon/HuixiangCommonApplicationTests.java
@@ -1,13 +1,5 @@
 package com.huixiang.huixiangcommon;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class HuixiangCommonApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
